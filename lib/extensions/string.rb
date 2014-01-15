47181314641921cc0d4fd7cc3ae8561b861458c5
Mike Piccolo <mfpiccolo@gmail.com>
2014-01-15 08:52:28 -0800

    Quick refactoring

--- lib/extensions/string.rb
+++ lib/extensions/string.rb
@@ -1,5 +1,21 @@
 class String
+
+  def vcr_path(example, spec_name)
+    self.scan(/^(.*?)::[abc]/) do |class_names|
+      class_name = class_names.flatten.first
+
+      if class_name.nil?
+        @path = example.class.name.prep
+      else
+        @path = example.class.name.gsub(class_name, "").prep.unshift(class_name)
+      end
+    end
+
+    @path.push(spec_name).join("/") unless @path.nil?
+  end
+
   def prep
     split("::").map {|e| e.sub(/[^\w]*$/, "")}.reject(&:empty?) - ["vcr"]
   end
+
 end
--- lib/minitest-vcr/spec.rb
+++ lib/minitest-vcr/spec.rb
@@ -7,26 +7,11 @@ module MinitestVcr
 
     def self.configure!
       run_before = lambda do |example|
-        if metadata[:vcr]
-
-          example.class.name.scan(/^(.*?)::[abc]/) do |class_names|
-            class_name = class_names.first
-
-            if class_name.nil?
-              @path = example.class.name.prep
-            else
-              @path = example.class.name.gsub(class_name, "").prep.unshift(class_name)
-            end
-          end
-
-          VCR.insert_cassette @path.push(spec_name).join("/") unless @path.nil?
-        end
+        VCR.insert_cassette example.class.name.vcr_path(example, spec_name) if metadata[:vcr]
       end
 
       run_after = lambda do |example|
-        if metadata[:vcr]
-          ::VCR.eject_cassette
-        end
+        ::VCR.eject_cassette if metadata[:vcr]
       end
 
       ::MiniTest::Spec.before :each, &run_before
